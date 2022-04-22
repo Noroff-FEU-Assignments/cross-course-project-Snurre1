5dc78c18305e1c73e229d04942e553e95bc315ad
Snurre1 <thomas.henriksen@stud.noroff.no>
2022-04-22 16:00:38 +0200

    s

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const url = "https://thomashenriksen.no/gamehub/wp-json/wc/store/products"
+const contain = document.querySelector(".product-list")
+async function getdata(){
+    try{
+        const response = await fetch(url)
+        const result = await response.json()
+
+        contain.innerHTML = "";
+
+        for (let i =0; i< result.length; i++){
+            console.log(result[i])
+
+            contain.innerHTML += `<div class= product-card>
+                                   <a href=details.html?id=${result[i].id}>
+                                    <div><h3>${result[i].name}</h3></div>
+                                   <div>
+                                    <img src="${result[i].images[0].src}"/></div>
+                                   </a>¨
+                                 <div class=productbtndiv>
+                                  <a href=details.html?id=${result[i].id}>
+                                   <button class=productbtn href=details.html?id=${result[i].id}>View details</button>
+                                   </a>
+                                 </div>
+                                </div>`
+        }
+    
+    
+    }catch(error){}
+
+
+}
+
+
+
+
+getdata()
