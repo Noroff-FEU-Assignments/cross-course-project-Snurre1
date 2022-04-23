065047e7bf806fe62d9ae32eacbee59afc046d85
Snurre1 <thomas.henriksen@stud.noroff.no>
2022-04-23 22:46:53 +0200

    finished

---
+++ js/details.js
@@ -0,0 +1,28 @@
+const container = document.querySelector("section")
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+const url = "https://thomashenriksen.no/gamehub/wp-json/wc/store/products/"+id;
+
+console.log(url)
+container.innerHTML = ""
+async function getGame(){
+
+    try{
+        const response = await fetch(url);
+        const result = await response.json();
+        console.log(result)
+        container.innerHTML =
+                               ` <div><h3>${result.name}</h3></div>
+                                <div><img src="${result.images[0].src}" alt="${result.images[0].alt}"></img>
+                                </div>
+                                <div><p>${result.description}</p></div>
+                                <div><p>${result.prices.price} ${result.prices.currency_code}</p></div>`
+                              
+
+
+    }catch(error){}
+}
+getGame();
